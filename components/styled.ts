4fd03febc7709ca548ead9d5189d76f2a2d94641
Pedro H G Migacz <pedromigacz@gmail.com>
2023-03-21 22:34:26 -0300

    add typography

---
+++ components/styled.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const HeadingLarge = styled.h1`
+  font-family: 'Fraunces 144pt';
+  font-weight: 600;
+  font-size: 80px;
+  line-height: 80px;
+`;
+
+export const HeadingMedium = styled.h2`
+  font-family: 'Fraunces 144pt';
+  font-weight: 600;
+  font-size: 56px;
+  line-height: 64px;
+`;
+
+export const HeadingSmall = styled.h3`
+  font-family: 'Fraunces 144pt';
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 40px;
+`;
+
+export const BodyText = styled.p`
+  font-family: 'Manrope';
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 32px;
+`;
