582b9daac47cdd4d8bf2cd41f18c3888e9d5c754
Pedro H G Migacz <pedromigacz@gmail.com>
2023-03-21 21:02:20 -0300

    add global styles

---
+++ components/styles/variables.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Variables = createGlobalStyle`
+  :root {
+    /* COLORS */
+    --dark-purple: #24053E;
+    --eucalyptus: #44FFA1;
+    --grey: #584D62;
+    --ghost-white: #FCF8FF;
+
+
+    /* FONTS */
+    
+    /* CONTAINERS */
+
+    --container-width: 1400px;
+
+
+    --rounded: 8px;
+  }
+`;
+
+export default Variables;
